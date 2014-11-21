d8cbcb5ae137ed8b146564e89890ee1ded8bad82
iplay88keys <iplay88keys@users.noreply.github.com>
2014-11-21 15:09:16 -0800

    merging

--- app/views/partials/table.blade.php
+++
@@ -1,4 +0,0 @@
-{{HTML::script('http://cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js')}}
-{{HTML::style('http://cdn.datatables.net/1.10.4/css/jquery.dataTables.css')}}
-{{HTML::script('http://cdn.datatables.net/tabletools/2.2.3/js/dataTables.tableTools.min.js')}}
-{{HTML::style('http://cdn.datatables.net/tabletools/2.2.3/css/dataTables.tableTools.css')}}
